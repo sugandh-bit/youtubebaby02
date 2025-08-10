eae7c6335b68a5c8a48dd16a4c39120591622997
sugandh-bit <mrsugandh32@gmail.com>
2025-08-10 23:48:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/mraryasingh/Babumusic01",
+    "https://github.com/sugandh-bit/youtubebaby02",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
